3ada999f9c8217fc833590ffac9253498657ff51
Rohit kumar <82631765+rohitkumar-7@users.noreply.github.com>
2025-04-18 13:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(value){
+		let year =parseInt(value);
+		let days ="";
+			if((year%4==0||year%400==0)&&year%100!=0){
+				days=366
+			}else{
+					days=365
+			}
+		return days
+}
